f4bb4cb9c5c40a9c30fa2f2d4863d6859fa4c86e
Lisa Nguyen <lisatramnguyen@yahoo.ca>
2022-09-24 04:32:55 -0400

    Updated main() to parse out student data and push it to a vector space

--- Assn2/source.cpp
+++ Assn2/source.cpp
@@ -1,10 +1,45 @@
 // Lisa Nguyen
 // CSCN73030 - Assignment 2
 // 09/23/22
-
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <vector>
 using namespace std;
+
+struct STUDENT_DATA
+{
+	string FirstName;
+	string LastName;
+};
 
 int main()
 {
+	string StudentDataFile = "StudentData.txt";
+	string str;
+	STUDENT_DATA student;
+	vector <STUDENT_DATA> DataVector;
+	ifstream file;
+	file.open(StudentDataFile);
+
+	if (file)
+	{
+		while (getline(file, str))
+		{
+			int comma = str.find(",");
+
+			student.LastName = str.substr(0, comma);
+			student.FirstName = str.substr(comma + 1, str.length());
+
+			DataVector.push_back(student);
+		}
+	}
+	else
+		cout << "Error opening file" << endl;
+
+	// printing from vector to console
+	/*for (STUDENT_DATA i : DataVector)
+		cout << i.FirstName << " " << i.LastName << endl;*/
+
 	return 1;
 }
